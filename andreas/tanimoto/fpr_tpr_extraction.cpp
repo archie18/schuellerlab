254e8d7876801d3a81d3bf21bff9851d0eb48190
mruiz <mruiz@flamingo.bioinfo.puc.cl>
2017-03-13 16:09:26 -0300

    last V of clustering scritp (actually running)

--- andreas/tanimoto/fpr_tpr_extraction.cpp
+++ andreas/tanimoto/fpr_tpr_extraction.cpp
@@ -51,51 +51,61 @@ vector<string> split(const string &s, char delim) {
     split(s, delim, back_inserter(elems));
     return elems;
 }
-/*
+
 struct roc_point
     {
         double true_positive_rate;
         double false_positive_rate;
         double detection_threshold;
     };
-*/
-int main(int argc, char** argv){
+
+void getRocCoordinates(string fileName){
 	vector<double> truep;
-	vector<double> pred;
-	//vector <double> roc_curve;
-	if(argc<=5){
-		ifstream ch15Fp2 (argv[1]);
-		//ifstream ch15Mol (argv[2]);
-		//ifstream ch22Fp2 (argv[3]);
-		//ifstream ch22Mol (argv[4]);
-		string line;
-		while(getline(ch15Fp2,line)){
-			vector<string> tokens = split(line,'\t');
-			string s = tokens[3];
-			//float tanNum = ConvertStringToFloat<float>(s);
-			if(isFloat(s) or s=="-99"){
-				double tanNum = stof (s);
-                        	double tpoNum = (double) stoull(tokens[6]);
-                                pred.push_back (tanNum);
-                                truep.push_back (tpoNum);
-			}
-			else{
-				cout << "Corrupt field value: " << s << " please check your input file." << endl;
-			}
+        vector<double> pred;
+	ifstream Chembl (fileName);
+	string line;
+	while(getline(Chembl,line)){
+                vector<string> tokens = split(line,'\t');
+                string s = tokens[3];
+		if(isFloat(s) or s=="-99"){
+                       	double tanNum = stof (s);
+			double tpoNum = (double) stoull(tokens[6]);
+			pred.push_back (tanNum);
+			truep.push_back (tpoNum);
+                }
+		else{
+			cout << "Corrupt field value: " << s << " please check your input file." << endl;
 		}
-		int sum = accumulate(truep.begin(), truep.end(), 0);
-		cout << "Number of Tps: " << sum << endl;
-		//float fpr;
-		//float tpr;
-		dlib::compute_roc_curve(truep,pred);   // DOESN'T HAVE ANY USAGE DOCUMENTATION ON INET
-		//cout << roc_curve.size() << endl;
-		//cout << setprecision(10);
-		//cout << "pred en posicion 0: " << pred[0] << endl << "tamaño vector truep: " << truep[0] << endl;
+		vector<dlib::roc_point> rocCurve = dlib::compute_roc_curve(truep,pred);
+		ofstream outFile;
+		string name;
+		name = fileName + ".fpr.tpr.coord";
+		outFile.open (name);
+		for (int i=0; i != rocCurve.size(); i++) {
+			double tpr;
+			double fpr;
+			double thr;
+			tpr = rocCurve[i].true_positive_rate;
+			fpr = rocCurve[i].false_positive_rate;
+			thr = rocCurve[i].detection_threshold;
+			outFile << tpr << "\t" << fpr << "\t" << thr << endl;
+		}
+		outFile.close();
+	}
+	int sum = accumulate(truep.begin(), truep.end(), 0);
+	cout << "Number of Tps in " << fileName << ": " << sum << endl;
+	Chembl.close();
+}
+
+int main(int argc, char** argv){
+	if(argc<=5){
+		for(int i=1; i<argc;i++){ 
+			getRocCoordinates(argv[i]);
+		}
+
 	}
 	else{
 		cout << "Missing argument?" << endl;
 	}
-
-	//extractFprTpr();
 	return 0;
 }
