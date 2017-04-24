a8078a99a78deda2a01f28e1620fb600214bd495
Andreas Schueller <apschueller@gmail.com>
2017-04-24 17:40:05 -0400

    Fixed error with label generation

--- andreas/tanimoto/test_silhouette/silh_test.py
+++ andreas/tanimoto/test_silhouette/silh_test.py
@@ -11,15 +11,18 @@ import sys
 def readmat(filename):
     with open(filename, 'rb') as f:
         data = np.fromfile(f, dtype=np.float32)
-        n = (1 + math.sqrt(1 + 8 * len(data))//2)
-        distmat = np.empty([int(n),int(n)])
-        for i in range(int(n)):
-            for j in range(int(n)):
-                distmat[i][j] = getdist(data,int(n),i,j)
-    return distmat
+        n = (1 + math.sqrt(1 + 8 * len(data))/2)
+        n = int(n)
+        distmat = np.empty([n,n])
+        for i in range(n):
+            for j in range(n):
+                distmat[i][j] = getdist(data,n,i,j)
+        return distmat,n
 
 ## Funcion para retornar el valor correspondiente a la coordenada
 def getdist(data, n, i, j):
+    #print ('n=', n, ' i=', i, ' j=', j)
+    #print (data)
     if i == j:
         return 1 # Return 1.0 for comparison of same indices
     # Flip indices
@@ -27,30 +30,25 @@ def getdist(data, n, i, j):
         temp = i
         i = j
         j = temp    
-    else:
-        return 1-data[i*n+j - (i+1)*(i+2)/2]
     
-    return 1-data[i*n+j - (i+1)*(i+2)/2]
+    return 1-data[int(i*n+j - (i+1)*(i+2)/2)]
 
 ## Ingreso de matriz y archivo clusters por parametro
-X=readmat(sys.argv[1])
+X,n=readmat(sys.argv[1])
 clusterfile=sys.argv[2]
 
 ## Lectura del archivo clusters para obtener el numero de clusters y generar lista con los labels (cluster correspondiente) para todos los ligandos
 with open(clusterfile) as clust:
-    line = clust.readlines()[1:]
+    lines = clust.readlines()[1:]
 
-n_clusters=len(line)    
-line = [x.strip() for x in line] 
-labels = []
-for i in line:
-    cluster,ligands=i.split(":")
-    lig=ligands[1:].split(" ")
-    if len(lig)>1:
-        for n in range(len(lig)):
-            labels.append(cluster)
-    else:
-        labels.append(cluster)
+n_clusters=len(lines)    
+lines = [x.strip() for x in lines] 
+labels = np.empty(n)
+for line in lines:
+    cluster,ligands=line.split(":")
+    ligs=ligands[1:].split(" ")
+    for lig in ligs:
+        labels[int(lig)] = cluster
 
 # The silhouette_score gives the average value for all the samples.
 # This gives a perspective into the density and separation of the formed
