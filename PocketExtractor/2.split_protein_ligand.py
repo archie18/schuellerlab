20dacf62d307734d0d7c34d9790e94d7e19a4ee0
Roberto <minho.roberto@gmail.com>
2017-05-10 18:34:20 -0300

    lala

--- PocketExtractor/2.split_protein_ligand.py
+++ PocketExtractor/2.split_protein_ligand.py
@@ -27,20 +27,23 @@ os.mkdir('./'+workdir+'/Ligands')
 tupla= []
 
 for algo in open(filename):
-	Codes = collections.namedtuple('Codes',['code', 'het','het_name','chain'])
+	Codes = collections.namedtuple('Codes',['code', 'het','het_name','chain','rsnum'])
 	algo = algo.replace('\n','') #remove if '\n'
 	s = algo.split(" ")
 	pdb=s[0]
 	het_file = s[1]+s[2]+s[3]+s[4]
 	cad = s[3]
 	het_nm = s[1]
+	resnum = s[2]
+	print resnum
 	pdb_filename = pdb
-	tupla.append(Codes(code=pdb.lower(),het=het_file, het_name=het_nm, chain=cad))
+	tupla.append(Codes(code=pdb.lower(),het=het_file, het_name=het_nm, chain=cad,rsnum=resnum))
 
 n = len(tupla)
 
 
 
 for l in tupla:
-	cmd = "python pdb_parser2.py "+l.code+".pdb "+l.het_name+" "+l.chain+" "+ workdir+" "+l.code+"_"+l.het_name 
-	os.system(cmd)
+	cmd = "python pdb_parser2.py "+l.code+".pdb "+l.het_name+" "+l.chain+" "+ workdir+" "+l.code+"_"+l.het_name+" "+l.rsnum
+	print cmd
+	#os.system(cmd)
--- PocketExtractor/pdb_parser2.py
+++ PocketExtractor/pdb_parser2.py
@@ -21,8 +21,8 @@ def openfile(filename):
 		return lines
 		
 def main():
-	if len(sys.argv) != 6:
-		print "Usage: pdb_parser2.py <pdb file> <hetatm name> <chain> <path> <name>"
+	if len(sys.argv) != 7:
+		print "Usage: pdb_parser2.py <pdb file> <hetatm name> <chain> <path> <name> <resnume>"
 		n = len(sys.argv)
 		print n
 		sys.exit()
@@ -32,18 +32,37 @@ def main():
 		chain = sys.argv[3]
 		path = sys.argv[4]
 		name = sys.argv[5]
+		resnume= sys.argv[6]
+		#print resnume
+		#print pdb_file
+		#print het_atm
+		#print chain
+		#print path
+		#print name
 		lig_path = "./"+path+"/Ligands/"
 		prot_path = "./"+path+"/Proteins/"
 
 		atm_protein = []
 		atm_ligand = []
+		
 		for line in pdb_file:
 			if line[0:4] == "ATOM":
 				atm_protein.append(line)
 			if line[0:6] =="HETATM":
 				if line[17:20] == het_atm:
 					if line[21:22] == chain:
-						atm_ligand.append(line)
+						if int(resnume) >= 1000:
+							if line[22:26]==resnume:
+								atm_ligand.append(line)
+						if int(resnume) >= 100:
+							if line[23:26]==resnume:
+								atm_ligand.append(line)
+						if int(resnume) >= 1000:
+							if line[24:26]==resnume:
+								atm_ligand.append(line)
+						else:
+							if line[25:26]==resnume:
+								atm_ligand.append(line)
 		atm_protein.append("TER")
 		atm_protein.append("END")
 		file_name = prot_path+name+".pdb"
@@ -53,6 +72,7 @@ def main():
 		atm_lig = []
 		for i in atm_ligand:
 			atm_lig.append(i[0:17]+"LIG     1     "+i[31:80])
+			#atm_lig.append(i[0:80])
 		file_name = lig_path+name+".pdb"
 		with open(file_name,'w') as out_file:
 			out_file.write('\n'.join(atm_lig))
