9fe44f9bd7225d053cb9bc59fd2b0102e4c523d0
Andreas Schueller <apschueller@gmail.com>
2017-06-10 10:34:21 -0400

    Improved version of split_single_multi.py

--- andreas/tanimoto/split_single_multi.py
+++ andreas/tanimoto/split_single_multi.py
@@ -1,10 +1,17 @@
 # split_single_multi.py
-# Version 0.1 2017-06-09
+#
+# HISTORY
+# 2017-09-10    0.2      Fixed bug with missing header line in output files
+#                        Renamed .multi to .multiple
+#                        Added interaction count output
+# 2017-06-09    0.1      Initial version
+#
+# AUTHOR
 # Andreas Schueller <aschueller@bio.puc.cl>
 
 import sys, os
 
-VERSION = '0.1'
+VERSION = '0.2'
 
 def main():
     print '%s v%s' % (os.path.basename(sys.argv[0]), VERSION)
@@ -14,16 +21,18 @@ def main():
     filename = sys.argv[1];
 
     singleout_filename = '%s.single' % filename 
-    multiout_filename = '%s.multi' % filename 
+    multiout_filename = '%s.multiple' % filename 
     
     # Count number of targets per ligand
     # Assumes that each ligid-targid pair is unique!
     targcount = {}
     data = {}
     skipfirst = True
+    header = ''
     with open(filename, 'r') as file:
         for line in file:
             if skipfirst:
+                header = line
                 skipfirst = False
                 continue
             tokens = line.split()
@@ -37,12 +46,16 @@ def main():
     # Sort interactions base on target counts and write to separate files
     single_count = 0
     multi_count = 0
+    multi_inter_count = 0
     with open(singleout_filename, 'w') as singleout:
         with open(multiout_filename, 'w') as multiout:
+            singleout.write(header)
+            multiout.write(header)
             for ligid in targcount:
                 if targcount[ligid] > 1:
                     multi_count += 1
                     for line in data[ligid]:
+                        multi_inter_count += 1
                         multiout.write(line)
                 else:
                     single_count += 1
@@ -50,6 +63,9 @@ def main():
     print 'Number of ligands with a single target:', single_count
     print 'Number of ligands with multiple targets:', multi_count
     print 'Total number of ligands:', (single_count + multi_count)
+    print 'Number of single interactions:', single_count
+    print 'Number of multiple interactions:', multi_inter_count
+    print 'Total number of interactions:', (single_count + multi_inter_count)
     
 if __name__ == "__main__":
     main()
