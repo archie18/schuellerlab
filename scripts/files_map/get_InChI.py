7d8de6636b365c91df4c932760a1435c0e8d073d
claudiio1 <claudioponce175@gmail.com>
2017-06-16 11:15:52 -0400

    Archivo para mapeo cruzado PDBbind vs ChEMBL

--- scripts/files_map/get_InChI.py
+++ scripts/files_map/get_InChI.py
@@ -1,5 +1,7 @@
-from urllib import urlopen
+import urllib,urllib2
 from bs4 import BeautifulSoup
+import pandas as pd
+from subprocess import call
 import argparse 
 
 parser = argparse.ArgumentParser()
@@ -13,18 +15,28 @@ for line in archivo:
 	if line[0]!='#':
 		prot = line.split()[0]
 		lig = line.split()[7].split('(')[1].split(')')[0]
-		proteins.append(prot)
-		if not lig in ligands:
-			ligands.append(lig)
+		if '-' in lig:
+			for e in lig.split('-'):
+				if not e in ligands:
+					ligands.append(e)
+			proteins.append(prot)
+		else:
+			if not lig in ligands:
+				ligands.append(lig)
+			proteins.append(prot)
 
 def get_InChI(ligand):
 	url ='http://ligand-expo.rcsb.org/pyapps/ldHandler.py?formid=cc-index-search&target='+ligand+'&operation=ccid'
-	html = urlopen(url).read()
+	html = urllib.urlopen(url).read()
 	raw = BeautifulSoup(html, "lxml")
 	txt=raw.get_text().split('\n')
 	for e in txt:
 		if 'InChI=' in e:
 			return e
+
+
 print 'Ligand InChI'
 for lig in ligands:
 	print lig, get_InChI(lig)
+
+
