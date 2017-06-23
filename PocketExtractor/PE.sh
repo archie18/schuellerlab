9cffcbe7d7e444fb7c2f2bec60d48303e1b46d16
Roberto <minho.roberto@gmail.com>
2017-06-23 13:04:00 -0400

    new excel

--- PocketExtractor/PE.sh
+++ PocketExtractor/PE.sh
@@ -1,6 +1,6 @@
 #!  /bin/bash
 
 cd $1
-moebatch -licwait -load ../PocketExtractor.svl -exec "PocketExtractor [ cutoff:6, proteinPath:'./Proteins', ligandPath:'./Ligands', write_ligand:1, ph4_atom_names:1, exclude_paths:[] , greasy:1, byRes:1];"
+moebatch -licwait -load ../PocketExtractor.svl -exec "PocketExtractor [ cutoff:6.0, proteinPath:'./Proteins', ligandPath:'./Ligands', write_ligand:1, ph4_atom_names:1, exclude_paths:[] , greasy:1, byRes:1];"
 
 
