7919cca8177edfd4148eb08b2c4c3e19dbfd32c4
rminho <minho.roberto@gmail.com>
2017-07-28 09:14:47 -0400

    script to load files to pymol

--- scripts/loadtopymol.py
+++ scripts/loadtopymol.py
@@ -13,18 +13,29 @@ import argparse
 import fnmatch
 
 parser = argparse.ArgumentParser()
-
+parser.add_argument('-c', '--code',    required=True, help='Protein code')
 parser.add_argument('-l', '--lig',    required=True, help='Ligand code')
 parser.add_argument('-p', '--path',    required=True, help='path where to find pockets')
 args = parser.parse_args()
 
-ligand = '*'+args.lig+'_pocket_ph4*'
+code = args.code
+ligand = code+'*'+args.lig+'_pocket_ph4*'+args.lig+'*.pdb'
 path = args.path
 pymol = 'pymol '
+
+sed = "sed 's/GRE/ C /g' | sed 's/DON/ N /g' | sed 's/ACC/ O /g' > "
+if not os.path.exists('./Temp'):
+	os.makedirs('./Temp')
+
+
 for file in os.listdir(path):
 	#print file
 	if fnmatch.fnmatch(file, ligand):
-		pymol = pymol + path +'/'+ file + ' '
+		pymol = pymol+'./Temp/'+ file + ' '
+		cat = 'cat ./'+path+'/'+ file + ' | '+ sed +'./Temp/'+ file 
 		#print file
-print pymol
+		#print cat
+		os.system(cat)
+#print pymol
 os.system(pymol)
+#cat 1OJD_LDA_pocket_ph4_gre_notByRes_6.0-3B6H_BOG_pocket_ph4_gre_notByRes_6.0.1.pdb | sed 's/GRE/ C /g' | sed 's/DON/ N /g' | sed 's/ACC/ O /g' > test.pdb
