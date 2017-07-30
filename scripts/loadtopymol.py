251425f0cf2fce4f60bea418e08200a4deb23132
rminho <minho.roberto@gmail.com>
2017-07-30 14:48:31 -0400

    adding new function to loadpymol.py

--- scripts/loadtopymol.py
+++ scripts/loadtopymol.py
@@ -3,11 +3,11 @@
 
 """
 Example:
-python loadtopymol.py -l PMP -p ./Homogeneous/Proteins/pockets
+python loadtopymol.py -c 1OJD -l PMP -p ./Homogeneous/Proteins/pockets
 Cargar los pockets de cierto ligando en pymol.
 """
 
-import os
+import os, os.path
 import sys
 import argparse
 import fnmatch
@@ -19,7 +19,9 @@ parser.add_argument('-p', '--path',    required=True, help='path where to find p
 args = parser.parse_args()
 
 code = args.code
-ligand = '*'+args.lig+'_pocket_ph4*'+args.code+'*'+args.lig+'*.pdb'
+
+ligandA = '*'+args.lig+'_pocket_ph4*'+args.code+'*'+args.lig+'*.pdb'
+ligandB = args.code+'_'+args.lig+'_pocket_ph4*'+args.lig+'*.pdb'
 query = args.code+'_'+args.lig+'_pocket_ph4*_6.0.pdb' # Will fail if 6.0 changes
 path = args.path
 pymol = 'pymol '
@@ -28,21 +30,45 @@ sed = "sed 's/GRE/ C /g' | sed 's/DON/ N /g' | sed 's/ACC/ O /g' > "
 if not os.path.exists('./Temp'):
 	os.makedirs('./Temp')
 
+path1= '/click_fold1'
+path2= '/click_fold2'
 
-for file in os.listdir(path):
+n_codes = len([file for file in os.listdir(path+path1) if fnmatch.fnmatch(file, ligandB)])
+print 'click fold 1'
+for file in os.listdir(path+path1):
 	#print file
-	if fnmatch.fnmatch(file, ligand):
+	#compares a single filename(ligand) against a pattern(file)
+	if fnmatch.fnmatch(file, ligandA):
 		pymol = pymol+'./Temp/'+ file + ' '
-		cat = 'cat ./'+path+'/'+ file + ' | '+ sed +'./Temp/'+ file 
+		cat = 'cat ./'+path+path1+'/'+ file + ' | '+ sed +'./Temp/'+ file 
+
+		if query < file:		
+			print "menor"
+		else:
+			print "mayor"
+		#print fileif
+		#print cat
+		#os.system(cat)
+print 'click fold 2'
+for file in os.listdir(path+path2):
+	#print file
+	#compares a single filename(ligand) against a pattern(file)
+	if fnmatch.fnmatch(file, ligandB):
+		pymol = pymol+'./Temp/'+ file + ' '
+		cat = 'cat ./'+path+path2+'/'+ file + ' | '+ sed +'./Temp/'+ file 
+
+		if query < file:		
+			print "menor"
+		else:
+			print "mayor"
 		#print file
 		#print cat
-		os.system(cat)
-
+		#os.system(cat)
 # Copy query pocket
-cat = 'cat ./'+path+'/'+ query + ' | '+ sed +'./Temp/'+ query
-os.system(cat)
-pymol += './Temp/'+ query
+cat = 'cat ./'+path+path1+'/'+ query + ' | '+ sed +'./Temp/'+ query
+#os.system(cat)
+pymol += './Temp/'+ query +' '
 
-#print pymol
-os.system(pymol)
+print pymol
+#os.system(pymol)
 #cat 1OJD_LDA_pocket_ph4_gre_notByRes_6.0-3B6H_BOG_pocket_ph4_gre_notByRes_6.0.1.pdb | sed 's/GRE/ C /g' | sed 's/DON/ N /g' | sed 's/ACC/ O /g' > test.pdb
