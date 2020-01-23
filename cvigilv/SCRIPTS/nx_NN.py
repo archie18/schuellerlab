a2b97dee76f4bb59bd477067bc849894bf058de7
Carlos Vigil Vásquez <cvigil2@uc.cl>
2020-01-23 13:26:43 -0300

    Added prediction generation in chunks (mainly to use in venetian)

--- cvigilv/SCRIPTS/nx_NN.py
+++ cvigilv/SCRIPTS/nx_NN.py
@@ -23,43 +23,7 @@ from multiprocessing import Pool
 from collections import defaultdict
 from configparser import ConfigParser
 
-def getCommunities(Graph):
-    # Get best partition and modularity
-    Partition  = cmt.best_partition(Graph, weight = 'weight', resolution = resolution)
-    Modularity = cmt.modularity(Partition, Graph)
-
-    CommunitiesList = set([id for n,id in Partition.items()])
-
-    # Create a histogram of the best partition
-    CommunitiesHistogram = defaultdict(list)
-    for N, ID in Partition.items():
-        CommunitiesHistogram[ID].append(N)
-
-    # Merge all communities with one lone member
-    Singletons = []
-    for ID in CommunitiesList:
-        if len(CommunitiesHistogram[ID]) == 1:
-            Singletons.append(*CommunitiesHistogram[ID])
-
-    # Change ID for lone members
-    for N, ID in Partition.items():
-        if N in Singletons:
-            Partition[N] = 'singleton'
-
-    # Assign community ID to each node in target graph
-    for Node, CommunityID in Partition.items():
-        Graph.nodes[Node]['community id'] = str(CommunityID)
-
-    return Modularity
-
 def predictRelation(inputs):
-    def path_cost(u,v,d):
-
-        node1_community = R.nodes[u]['community id']
-        node2_community = R.nodes[v]['community id']
-        weight          = d['weight']
-        return weight * alpha + math.exp(1-(len(set([node1_community, node2_community])))) * (1-alpha)
-   
     Graph       = ML
     source_node = inputs[0]
     target_node = inputs[1]
@@ -73,7 +37,10 @@ def predictRelation(inputs):
 
     # Cycle through edges weight to get the minimum value
     try:
-        score, path = nx.single_source_dijkstra(R, source_node, target_node, weight=path_cost)
+        score, path = nx.single_source_dijkstra(R, \
+                source = source_node, \
+                target = target_node, \
+                weight = 'weight')
         step_node = path[1]
     except nx.exception.NetworkXNoPath:
         pass
@@ -98,14 +65,14 @@ if __name__ == '__main__':
     target_layer = config.get('Options','Target layer')
 
     # Assign fold to source layer nodes
-    #print(f'Assigning fold to the nodes in layer "{source_layer}"')
-    #for i, n in enumerate([n for n,d in ML.nodes(data=True) if d['layer'] == source_layer]):
-    #    ML.nodes[n]['fold'] = i % 10
+    print(f'Assigning fold to the nodes in layer "{source_layer}"')
+    for i, n in enumerate([n for n,d in ML.nodes(data=True) if d['layer'] == source_layer]):
+        ML.nodes[n]['fold'] = i % 10
     # This snippet is used to validate if the script is predicting correctly 
-    with open('/home/cvigilv/Repos/schuellerlab/cvigilv/EXTRAS/correctFolds.csv') as folds_file:
-        for line in folds_file:
-            fold, node= line.rstrip().split(',')
-            ML.nodes[node]['fold']=int(fold)
+    #with open('/home/cvigilv/Repos/schuellerlab/cvigilv/EXTRAS/correctFolds.csv') as folds_file:
+    #    for line in folds_file:
+    #        fold, node= line.rstrip().split(',')
+    #        ML.nodes[node]['fold']=int(fold)
 
     # Get some data before generating folds
     target_nodes = [n for n,d in ML.nodes(data=True) if d['layer']==target_layer]
@@ -131,7 +98,6 @@ if __name__ == '__main__':
 
         # Remove edges from the test set that connect layers
         # (This are the edges we want to predict!)
-        # TODO: Cambiar nombres de variables ('DTIs'-like) a algo más genérico.
         relations_eliminated = 0
         fold_relations = []
         for n1 in fold_source_nodes:
@@ -142,18 +108,15 @@ if __name__ == '__main__':
             ML.remove_edges_from(relations)
         print(f'Fold {fold} "{rel2pred}" edges: {relations_eliminated}')
         
-        # Get community organization for fold graph
-        modularity = getCommunities(ML)
-        print(f'Fold {fold} modularity: {modularity}')
+        # Generate predictions in chunks
+        for chunk in tqdm(np.array_split(edges2predict, 1000), total=1000):
+            Proccess = Pool(n_processes)
+            Predictions_raw = Proccess.map(predictRelation, chunk)
+            Proccess.close()
 
-        # Generate predictions using nearest-neighbour implementation
-        Proccess = Pool(n_processes)
-        Predictions_raw = Proccess.map(predictRelation, edges2predict)
-        Proccess.close()
-
-        # Write predictions to output file
-        for Prediction in Predictions_raw:
-            print('\t'.join([str(element) for element in Prediction]), file = out_file, flush=True)
+            # Write predictions to output file
+            for Prediction in Predictions_raw:
+                print('\t'.join([str(element) for element in Prediction]), file = out_file, flush=True)
 
         print(f'Fold {fold} done! Time elapsed: {datetime.datetime.now() - time}')
 
