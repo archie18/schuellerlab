088045cb7de35eae1f3334c9d8f4827566e2ba82
Carlos Vigil Vásquez <cvigil2@uc.cl>
2020-04-09 00:17:36 -0400

    Initial commi

--- cvigilv/SCRIPTS/ig_DASPfind.py
+++ cvigilv/SCRIPTS/ig_DASPfind.py
@@ -4,16 +4,18 @@
 # ig_DASPfind.py
 # by Carlos Vigil, Andreas Schüller
 #
-# Reimplementación de DASPfind (Ba-Alawi, 2016). Método predictivo
-# de interacciones proteína-ligando basado en formalismo de redes.
-# HISTORY:  0.0  CVV 2020.04.06   First version
+# Reimplementation of Ba-Alawi predictive method based in all
+# simple paths.
+#
+# HISTORY:  0.0 CVV 2020.04.09  First version
 # =============================================================
 
-__version__ = 0.0
-__title__   = f'ig_DASPfind.py - v{__version__})'
+__version__ = '0.0'
+__title__   = f'ig_DASPfind.py (v{__version__})'
 
 import os
 import sys
+import random
 import datetime
 import numpy as np
 from igraph import *
@@ -21,59 +23,40 @@ from tqdm import tqdm
 from multiprocessing import Pool
 from configparser import ConfigParser
 
-def getFoldRelations(n1):
-    """Get list of edges to predict in fold.
-
-    Args:
-        n1 (str): Source node ID
-    """
-    return [(n1, n2) for n2 in ML.neighborhood(n1) if ML.vs[n2]['layer'] == target_layer]
-
 def predictRelation(inputs):
-    """Predict protein-ligand interactions using Dijkstra shortest path algrithm.
+    """Predict protein-ligand interactions using all simple paths of n lenght.
 
     Find shortest path between a source node and a target node. Returns an ordered
-    list with (i) fold ID, (ii) source node ID, (iii) target node ID, (iv) Dijkstra
-    path lenght or score, (v) node IDs of shortest path, (vi) edge weights of
-    shortest path, (vii) relation between source and target node and (viii) number
-    of steps / path length.
+    list with (i) fold ID, (ii) source node ID, (iii) target node ID, (iv) DASPfind
+    score, (v) number of paths used, (vi) EMPTY (vii) relation between source and
+    target node.
 
     Args:
         inputs (tuple): Node pair in format (source_node, target_node)
     """
     source_node = inputs[0]
     target_node = inputs[1]
+    score = 0
 
-    try:
-        # Get shortest path from source to target node
-        paths = ML.get_all_simple_paths(v       = source_node, \
-                                       to      = target_node, \
-                                       cutoff  = 4)
+    # Get shortest path from source to target node
+    paths = ML.get_all_simple_paths(v      = source_node, \
+                                    to     = target_node, \
+                                    cutoff = config.getint('Options','Steps cutoff'))
 
-        # Calculate score and metrics of shortest path
-        scores = []
-        for path in paths:
-            path_edges   = [ML.get_eid(u,v) for u,v in list(zip(path[:-1],path[1:]))]
-            path_weights = [1-ML.es[e]['weight'] for e in path_edges if ML.es[e]['weight'] != 1.0]
-            path_score   = np.prod(path_weights)**(2.26*len(path_weights))
-            scores.append(path_score)
-        score = sum(scores)
-
-    except:
-        # If no path found, use dummy values
-        path_ids     = [source_node,'Null',target_node]
-        path_edges   = 'NaN'
-        path_weights = 'NaN'
-        path_length  = 'NaN'
-        score        = -99
+    # Calculate score and metrics of shortest path
+    for path in paths:
+        path_weights = [ML.es[e]['sim'] for e in path]
+        path_length  = len(path_weights)
+        path_score   = np.prod(path_weights)**(2.26*path_length)
+        score += path_score
 
     return ML.vs[source_node]['fold'], \
            ML.vs[source_node]['id'], \
            ML.vs[target_node]['id'], \
            str(score), \
+           len(paths), \
            '', \
-           '', \
-           str(bool((source_node,target_node) in fold_relations)), \
+           str(TP)
 
 if __name__ == '__main__':
     print(__title__)
@@ -85,6 +68,13 @@ if __name__ == '__main__':
     # Read multilayered graph into memory and print basic stats of it.
     print('Reading multilayered graph into memory')
     ML = Graph.Read_GraphML(config.get('I/O','Multilayered graph file'))
+    rel2pred    = config.get('Options', 'Relation to predict')
+
+    # Set LT relation to 1
+    print('Changing {rel2pred} similarity weight for')
+    for e in ML.es:
+        if e['relation'] == 'LT':
+            e['sim'] = 1
 
     # Get source and target layers to generate predictions
     source_layer = config.get('Options','Source layer')
@@ -102,7 +92,7 @@ if __name__ == '__main__':
     elif crossvalidation == 'LOOCV':
         for i, n in enumerate(source_nodes):
             n['fold']  = i
-        fold_count = i+1
+        fold_count = len(source_nodes)
     elif os.path.isfile(crossvalidation):
         with open(crossvalidation,'r') as folds_file:
             for line in folds_file:
@@ -115,57 +105,50 @@ if __name__ == '__main__':
             n['fold'] = i % 10
         fold_count = 10
 
+    # Reduce edges from layer
+    sim_cutoff = config.get('Options', 'Similarity cutoff')
+    if sim_cutoff != 'False':
+        if sim_cutoff == 'Sparsest':
+            mst_edges        = nx.maximum_spanning_edges(ML, weight='sim', data=True)
+            mst_edges_weight = [float(d['sim']) for u,v,d in mst_edges]
+            max_weight       = max(mst_edges_weight)
+        elif 0.0 < float(sim_cutoff) < 1.0:
+            max_weight   = float(sim_cutoff)
+
+        ML.delete_edges([e for e in ML.es if e['sim']<max_weight])
+
     # Generate predictions
     time        = datetime.datetime.now()
     out_file    = open(config.get('I/O','Output predictions file'),"w+")
-    rel2pred    = config.get('Options', 'Relation to predict')
     n_processes = config.getint('I/O', 'Number of parallel processes')
     n_chunks    = config.getint('I/O', 'Number of chunks per process')
-    weight_used = config.get('Options','Edge weight')
+    weight_used = edge_weights_options[config.get('Options','Edge weight')]
 
-    for fold in tqdm(range(fold_count), total=fold_count):
-        print(f'Generating predictions for fold {fold}')
-        fold_source_nodes = [n.index for n in ML.vs \
-                                     if  n['layer']==source_layer and \
-                                         n['fold']==fold]
-        edges2predict     = [(n1,n2) for n1 in fold_source_nodes \
-                                     for n2 in target_nodes]
+    source_nodes = [n.index for n in source_node]
+    edges2predict= [(n1,n2) for n1 in source_nodes for n2 in target_nodes]
 
-        # Remove edges from the test set that connect layers
-        # (This are the edges we want to predict!)
-        # Parallel processes to make it faster
-        delete_edges2predict = Pool(n_processes)
-        fold_relations = delete_edges2predict.map(getFoldRelations, fold_source_nodes)
-        delete_edges2predict.close()
-        fold_relations = [item for sublist in fold_relations for item in sublist]
-        ML.delete_edges(fold_relations)
+    # Iterate over all possible 'rel2pred' edges
+    for s,t in tqdm(edges2predict, total=len(edges2predict)):
+        TP = False
+        if ML.are_connected(s, t):
+            TP = True
 
-        print(f'Fold {fold} "{rel2pred}" edges: {len(fold_relations)}')
+            # Remove edges from the test set that connect layers
+            ML.delete_edges([(s,t)])
 
-        ## Calculate clusters/communities for graph
-        #communities = ML.community_infomap(edge_weights='weight')
-        #for n_comm, community in enumerate(communities):
-        #    for node in community:
-        #        ML.vs[node]['community'] = n_comm
-        #print(f"Modularity for fold clustering: {communities.modularity}")
+        # Write predictions to output file
+        Prediction = predictRelation([s,t])
+        print('\t'.join([str(element) for element in Prediction]), file = out_file, flush=True)
 
-        # Generate predictions in chunks
-        for chunk in np.array_split(edges2predict, n_chunks):
-            Predict = Pool(n_processes)
-            Predictions_raw = Predict.map(predictRelation, chunk)
-            Predict.close()
-
-            # Write predictions to output file
-            for Prediction in Predictions_raw:
-                print('\t'.join([str(element) for element in Prediction]), file = out_file, flush=True)
-
-        # Add interlayer edges back to the multilayered graph.
-        ML.add_edges(fold_relations)
-        for u,v in fold_relations:
-            ML.es[ML.get_eid(u,v)]['relation'] = rel2pred
-            if weight_used == 'Distance':
-                ML.es[ML.get_eid(u,v)]['weight'] = float(1)
-            elif weight_used == 'pSimilarity':
-                ML.es[ML.get_eid(u,v)]['weight'] = float(10)
+        # Add 'rel2pred' edges back to the multilayered graph.
+        if TP:
+            # Restore edge information to deleted DTI
+            ML.add_edge(s,t)
+            ML.es[ML.get_eid(s,t)]['relation']   = rel2pred
+            ML.es[ML.get_eid(s,t)]['distance']   = float(1)
+            ML.es[ML.get_eid(s,t)]['sim']        = float(1)
+            ML.es[ML.get_eid(s,t)]['p_distance'] = float(0)
+            ML.es[ML.get_eid(s,t)]['p_sim']      = float(0)
 
     print(f'Predictions done! Time elapsed: {datetime.datetime.now() - time}')
+    out_file.close()
