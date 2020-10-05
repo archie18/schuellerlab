1ebf97204b23bd15b4be3f42ae4626884199d717
Carlos Vigil Vásquez <cvigil2@uc.cl>
2020-10-05 16:15:03 -0300

    C++ implementation of matrices for *de novo* NBI

---
+++ cvigilv/denovoNBI/matrix.h
@@ -0,0 +1,58 @@
+#ifndef __EE_242_Project_2__matrix__
+#define __EE_242_Project_2__matrix__
+
+#include <stdio.h>
+#include <fstream> // for file access
+#include <iostream>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <cmath>
+
+using std::vector;
+using std::tuple;
+
+class Matrix {
+	private:
+		unsigned m_rowSize;
+		unsigned m_colSize;
+		vector<vector<double> > m_matrix;
+	
+	public:
+		// Matrix constructors
+		Matrix(unsigned, unsigned, double);
+		Matrix(const char *);
+
+		// Matriz Generators
+		Matrix Random(unsigned, unsigned, double) const;
+
+		// Matrix IO
+		void write(const char *, const char) const;
+
+		// Matrix operations
+		Matrix operator+(Matrix &);
+		Matrix operator-(Matrix &);
+		Matrix operator*(Matrix &);
+		Matrix transpose();
+
+		// Scalar operations
+		Matrix operator+(double);
+		Matrix operator-(double);
+		Matrix operator*(double);
+		Matrix operator/(double);
+
+		// Aesthetic methods
+		double& operator()(const unsigned &, const unsigned &);
+		void print() const;
+		unsigned getRows() const;
+		unsigned getColumns() const;
+		//tuple<unsigned, unsigned> getSize() const;
+
+		// Graph methods
+		vector<unsigned> neighbours(const unsigned &, const char) const;
+		unsigned degree(const int &, const char) const;
+};
+
+#endif /* defined(__EE_242_Project_2__matrix__) */
---
+++ cvigilv/denovoNBI/test.cpp
@@ -0,0 +1,17 @@
+#include <fstream> // for file access
+#include <iostream>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+#include <cmath> // For Absolute function
+#include "matrix.h" // Matrix Class
+
+int main() {
+
+	Matrix A(10,10,0.0);
+
+	A.print();
+
+	return 0;
+}
+    
